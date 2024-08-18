01ed5a5084b8d6ac2ce169b60593a5b83540c0f5
Michael <michaelhu1@icloud.com>
2024-08-19 00:27:52 +0800

    Update route.ts

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,49 +1,31 @@
-import { type NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 import Mail from 'nodemailer/lib/mailer';
 
 export async function POST(request: NextRequest) {
-  const { email, name, message } = await request.json();
+  try {
+    const { email, name, message } = await request.json();
 
-  const transport = nodemailer.createTransport({
-    service: 'gmail',
-    /* 
-      setting service as 'gmail' is same as providing these setings:
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true
-      If you want to use a different email provider other than gmail, you need to provide these manually.
-      Or you can go use these well known services and their settings at
-      https://github.com/nodemailer/nodemailer/blob/master/lib/well-known/services.json
-  */
-    auth: {
-      user: process.env.MY_EMAIL,
-      pass: process.env.MY_PASSWORD,
-    },
-  });
-
-  const mailOptions: Mail.Options = {
-    from: process.env.MY_EMAIL,
-    to: process.env.MY_EMAIL,
-    subject: `Message from ${name} (${email}) from Girl Talk Website`,
-    text: message,
-  };
-
-  const sendMailPromise = () =>
-    new Promise<string>((resolve, reject) => {
-      transport.sendMail(mailOptions, function (err) {
-        if (!err) {
-          resolve('Email sent');
-        } else {
-          reject(err.message);
-        }
-      });
+    const transport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.MY_EMAIL,
+        pass: process.env.MY_PASSWORD,
+      },
     });
 
-  try {
-    await sendMailPromise();
-    return NextResponse.json({ message: 'Message recieved' });
-  } catch (err) {
-    return NextResponse.json({ error: err }, { status: 500 });
+    const mailOptions: Mail.Options = {
+      from: process.env.MY_EMAIL,
+      to: process.env.MY_EMAIL,
+      subject: `Message from ${name} (${email}) from Girl Talk Website`,
+      text: message,
+    };
+
+    await transport.sendMail(mailOptions);
+
+    return NextResponse.json({ message: 'Message received' });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return NextResponse.json({ error: 'Failed to send email' }, { status: 500 });
   }
 }
